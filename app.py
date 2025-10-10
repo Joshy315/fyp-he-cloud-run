cd60d6fa7bab7f650a1c3186f5c02b7fb0ff9790
Joshy315 <haziqsabri8@gmail.com>
2025-10-10 12:05:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask, request, jsonify
 import seal
@@ -9,20 +8,19 @@ import time
 
 app = Flask(__name__)
 
-# This function must now also use files to load the data
+# This function uses temporary files to load data correctly
 def deserialize_from_base64(encoded_string, target_class, context=None, filename="temp_server_object"):
     bytes_data = base64.b64decode(encoded_string)
     with open(filename, 'wb') as f:
         f.write(bytes_data)
     
     new_object = target_class()
-    # EncryptionParameters.load() is special and doesn't need a context
     if target_class == EncryptionParameters:
         new_object.load(filename)
     else:
         new_object.load(context, filename)
     
-    os.remove(filename) # Clean up
+    os.remove(filename)
     return new_object
 
 @app.route('/compute_average', methods=['POST'])
@@ -30,13 +28,12 @@ def compute_average():
     data = request.json
     try:
         # 1. Recreate SEAL Context
-        # We give each deserialization a unique temp filename to avoid conflicts
         parms = deserialize_from_base64(data['parms'], EncryptionParameters, filename="temp_s_parms")
         context = SEALContext(parms)
         ckks_encoder = CKKSEncoder(context)
         evaluator = Evaluator(context)
         
-        # 2. Deserialize all required data from client
+        # 2. Deserialize all data from the client
         cloud_cipher = deserialize_from_base64(data['cipher_data'], Ciphertext, context, "temp_s_cipher")
         cloud_galois_keys = deserialize_from_base64(data['galois_keys'], GaloisKeys, context, "temp_s_galois")
         cloud_relin_keys = deserialize_from_base64(data['relin_keys'], RelinKeys, context, "temp_s_relin")
@@ -44,23 +41,20 @@ def compute_average():
         
         start_time = time.time()
         
-        # --- HEAVY COMPUTATION ---
-        # Sum all elements in the encrypted vector
+        # --- Heavy Homomorphic Computation ---
         rotated_ciphers = [evaluator.rotate_vector(cloud_cipher, i, cloud_galois_keys) for i in range(sample_size)]
         total_sum_cipher = evaluator.add_many(rotated_ciphers)
         
-        # Divide to get the average
         scale = total_sum_cipher.scale()
         division_plain = ckks_encoder.encode(1.0 / sample_size, scale)
         avg_cipher = evaluator.multiply_plain(total_sum_cipher, division_plain)
         
-        # Relinearize after multiplication
         evaluator.relinearize_inplace(avg_cipher, cloud_relin_keys)
         evaluator.rescale_to_next_inplace(avg_cipher)
         
         processing_time = (time.time() - start_time) * 1000
 
-        # 3. Serialize and return result
+        # 3. Serialize and return the result
         result_filename = "temp_s_result"
         avg_cipher.save(result_filename)
         with open(result_filename, 'rb') as f:
@@ -73,7 +67,6 @@ def compute_average():
             'cloud_processing_time_ms': processing_time
         })
     except Exception as e:
-        # Provide a more detailed error message for debugging
         return jsonify({'error': str(e), 'traceback': repr(e)}), 500
 
 if __name__ == "__main__":
